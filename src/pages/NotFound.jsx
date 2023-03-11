e0eb7689a47a0f243464ae0ef845765b216ecd5c
Yusuf <baliyusuf675@gmail.com>
2023-03-11 18:49:00 +0300

    notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import hata from "../assets/404.png";
 const NotFound = () => {
   return (
     <div>
-      <h5>Aradığınız sayfaya bulunamadı!</h5>
+      <h5>Aradığınız Sayfa Bulunamadı!</h5>
       <img src={hata} alt="" />
     </div>
   );
