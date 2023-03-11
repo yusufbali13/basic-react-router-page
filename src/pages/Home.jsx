23bec3aac6d9f11e6f7560d6d77d4c0095bff611
Yusuf <baliyusuf675@gmail.com>
2023-03-11 17:08:26 +0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import Header from "../components/Header";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <div>
+      <div className="banner">
+        <h1>Professional Web Design</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          luctus ipsum, rhoncus semper magna. Nulla nec magna sit amet sem
+          interdum condimentum.
+        </p>
+      </div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
